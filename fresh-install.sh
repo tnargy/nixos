b399b093013400ced0b3ceb240121afbbac5ef4f
onyx <clsscmstng@gmail.com>
2022-09-11 00:52:28 -0500

    removed install script

--- fresh-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sudo su
-nix-env -iA nixos.git
-git clone https://github.com/tnargy/nixos
-cd nixos
-cp /etc/nixos/hardware-configuration.nix system/
-nixos-install --flake .#nixos
-reboot
