93f796b72dbd696cc78cbdfbe2e923011c560f5d
onyx <clsscmstng@gmail.com>
2022-09-25 23:11:45 -0500

    added tmux config

--- home/modules/profiles/base/default.nix
+++ home/modules/profiles/base/default.nix
@@ -49,6 +49,21 @@ in {
         compression = true;
       };
 
+      tmux = {
+        enable = true;
+        newSession = true;
+        escapeTime = 10;
+        keyMode = "vi";
+        extraConfig = ''
+          bind-key j command-prompt -p "join pane from:" "join-pane -s '%%'"
+          bind-key s command-prompt -p "send pane to:" "join-pane -t '%%'"
+          bind | split-window -h
+          bind - split-window -v
+          unbind '"'
+          unbind %
+          bind r source-file ~/.config/tmux/tmux.conf
+        '';
+      };
     };
     services = {
       gpg-agent = {
