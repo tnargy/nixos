f8b1109f221dd202f0520579c3d4c6da9c194c44
onyx <clsscmstng@gmail.com>
2022-09-26 19:49:12 -0500

    setting wallpaper

--- home/modules/misc/awesome/default.nix
+++ home/modules/misc/awesome/default.nix
@@ -235,7 +235,7 @@ in {
       local function set_wallpaper(s)
          -- Wallpaper
          if beautiful.wallpaper then
-            local wallpaper = beautiful.wallpaper
+            local wallpaper = "~/.nixos/wallpaper.jpg" #beautiful.wallpaper
             -- If wallpaper is a function, call it with the screen
             if type(wallpaper) == "function" then
                wallpaper = wallpaper(s)
