0d6b5bb0013a3b1b4f1e8a972edc168db7bea631
onyx <clsscmstng@gmail.com>
2022-10-01 22:23:58 -0500

    added alacrity theme

--- home/modules/profiles/base/default.nix
+++ home/modules/profiles/base/default.nix
@@ -23,7 +23,6 @@ in {
 
     programs = {
       advancedCopy.enable = true;
-      alacritty.enable = true;
       bat.enable = true;
       direnv.enable = true;
       direnv.nix-direnv.enable = true;
@@ -33,6 +32,41 @@ in {
       jq.enable = true;
       neovim.enable = true;
       starship.enable = true;
+
+      alacritty = {
+        enable = true;
+        settings = {
+          font.size = 12;
+          colors = {
+            primary = {
+              background = "0x282828";
+              foreground = "0xdfbf8e";
+            };
+
+            normal = {
+              black=   "0x665c54";
+              red=     "0xea6962";
+              green=   "0xa9b665";
+              yellow=  "0xe78a4e";
+              blue=    "0x7daea3";
+              magenta= "0xd3869b";
+              cyan=    "0x89b482";
+              white=   "0xdfbf8e";
+            };
+
+            bright = {
+              black=   "0x928374";
+              red=     "0xea6962";
+              green=   "0xa9b665";
+              yellow=  "0xe3a84e";
+              blue=    "0x7daea3";
+              magenta= "0xd3869b";
+              cyan=    "0x89b482";
+              white=   "0xdfbf8e";
+            };
+          };
+        };
+      };
 
       gpg = {
         enable = true;
