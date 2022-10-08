943f0ce3de13115f468e87be2b9359dfa9bd9f80
tnargy <clsscmstng@gmail.com>
2022-10-08 16:23:53 -0500

    Adding geek configuration

--- nixos/configurations/default.nix
+++ nixos/configurations/default.nix
@@ -1,4 +1,5 @@
 {self, ...} @ inputs: {
   nixos = self.lib.mkSystem "nixos" inputs.nixpkgs;
   surface = self.lib.mkSystem "surface" inputs.nixpkgs;
+  geek = self.lib.mkSystem "geek" inputs.nixpkgs;
 }
