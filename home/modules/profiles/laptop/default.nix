48cf64b0538fd1ca33781e50c272eac4329eb930
onyx <clsscmstng@gmail.com>
2022-10-10 00:07:40 -0500

    removed xdg.configFile

--- home/modules/profiles/laptop/default.nix
+++ home/modules/profiles/laptop/default.nix
@@ -14,12 +14,5 @@ in {
 
   config = lib.mkIf cfg.enable {
     home.packages = with pkgs; [discord spotify-unwrapped];
-
-    xdg.configFile."awesome/battery-widget".source = builtins.fetchFromGitHub {
-      owner = "deficient";
-      repo = "battery-widget";
-      rev = "25b7e94a34ed854697d61e3cbb0a3ebd9745dbd4";
-      sha256 = "";
-    };
   };
 }
