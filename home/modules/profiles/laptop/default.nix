4719585544d896fbbffdb592de77b0a808becfd7
onyx <clsscmstng@gmail.com>
2022-10-10 22:48:59 -0500

    fetchFromGitHub battery-widget

--- home/modules/profiles/laptop/default.nix
+++ home/modules/profiles/laptop/default.nix
@@ -5,6 +5,12 @@
   ...
 }: let
   cfg = config.profiles.laptop;
+  battery-widget = pkgs.fetchFromGitHub {
+    owner = "deficient";
+    repo = "battery-widget";
+    rev = "25b7e94a34ed854697d61e3cbb0a3ebd9745dbd4";
+    sha256 = "sha256-cdqvPyDL3TW0mJugXQuwPkCqf6474M/wka6jWi7IevI=";
+  };
 in {
   options.profiles.laptop = {
     enable =
@@ -14,5 +20,8 @@ in {
 
   config = lib.mkIf cfg.enable {
     home.packages = with pkgs; [discord spotify-unwrapped];
+    home.file = {
+      ".config/awesome/battery-widget".source = "${battery-widget}";
+    };
   };
 }
