381e208eec6f430b15d9a0ce97a8cdf9e06e27a4
onyx <clsscmstng@gmail.com>
2022-11-23 09:38:03 -0600

    Removed Surface Config

--- home/configurations/default.nix
+++ home/configurations/default.nix
@@ -1,5 +1,4 @@
 {self, ...} @ inputs: {
   "onyx@nixos" = self.lib.mkHome "onyx" "nixos" "x86_64-linux" inputs.nixpkgs;
-  "onyx@surface" = self.lib.mkHome "onyx" "surface" "x86_64-linux" inputs.nixpkgs;
   "onyx@geek" = self.lib.mkHome "onyx" "geek" "x86_64-linux" inputs.nixpkgs;
 }
--- nixos/configurations/default.nix
+++ nixos/configurations/default.nix
@@ -1,5 +1,4 @@
 {self, ...} @ inputs: {
   nixos = self.lib.mkSystem "nixos" inputs.nixpkgs;
-  surface = self.lib.mkSystem "surface" inputs.nixpkgs;
   geek = self.lib.mkSystem "geek" inputs.nixpkgs;
 }
