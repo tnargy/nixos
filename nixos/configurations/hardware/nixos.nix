865169def9de3a4ff78f030dc3defc5317b39d6b
onyx <clsscmstng@gmail.com>
2023-09-06 18:56:57 -0500

    updated hardware and lock

--- nixos/configurations/hardware/nixos.nix
+++ nixos/configurations/hardware/nixos.nix
@@ -6,36 +6,33 @@
 {
   imports = [ ];
 
-  boot.initrd.availableKernelModules = [ "ata_piix" "ohci_pci" "ehci_pci" "sd_mod" "sr_mod" ];
+  boot.initrd.availableKernelModules = [ "ata_piix" "ohci_pci" "ehci_pci" "ahci" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/sda2";
-      fsType = "ext4";
+    { device = "/dev/sda3";
+      fsType = "btrfs";
     };
 
-  # fileSystems."/boot" =
-  #   { device = "/dev/sda1";
-  #     fsType = "vfat";
-  #   };
+  fileSystems."/boot" =
+    { device = "/dev/sda1";
+      fsType = "ext4";
+      noCheck = true;
+    };
 
-  swapDevices = [ ];
+  swapDevices =
+    [ { device = "/dev/sda2"; }
+    ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  # networking.useDHCP = lib.mkDefault true;
-  networking.interfaces.enp0s3.ipv4.addresses = [ {
-	address = "10.0.2.38";
-	prefixLength = 24;
-  } ];
-  networking.defaultGateway = "10.0.2.1";
-  networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp0s3.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   virtualisation.virtualbox.guest.enable = true;
 }
